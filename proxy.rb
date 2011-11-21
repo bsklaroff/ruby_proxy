e7a29d69d2f6ea737db32de8f2cb880a71a8f34c
Ben Sklaroff <bsklaroff@gmail.com>
2011-11-20 16:25:01 -0800

    works pretty well

--- proxy.rb
+++ proxy.rb
@@ -3,38 +3,19 @@ require 'net/http'
 
 server = TCPServer.new(2347)
 puts "hello"
-while (session = server.accept)
- #start new thread conversation
- ## Here we will establish a new thread for a connection client
- Thread.start do
-   ## I want to be sure to output something on the server side
-   ## to show that there has been a connection
-   puts "log: Connection from #{session.peeraddr[2]} at
-          #{session.peeraddr[3]}"
-   puts "log: got input from client"
-   ## lets see what the client has to say by grabbing the input
-   ## then display it. Please note that the session.gets will look
-   ## for an end of line character "\n" before moving forward.
-   input = session.gets
-   puts input
-  response = Net::HTTP.get(URI(input.split(' ')[1]))
-  puts response
-   ## Lets respond with a nice warm welcome message
-   session.puts response
- end  #end thread conversation
-end   #end loop
-=begin
-host = 'www.tutorialspoint.com'     # The web server
-port = 80                           # Default HTTP port
-path = "/index.htm"                 # The file we want 
-
-# This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
-
-socket = TCPSocket.open(host,port)  # Connect to server
-socket.print(request)               # Send request
-response = socket.read              # Read complete response
-# Split response at first blank line into headers and body
-headers,body = response.split("\r\n\r\n", 2) 
-print body                          # And display it
-=end
+while true
+  Thread.start(server.accept) do |session|
+    puts "log: got input from client"
+    input = session.gets
+    puts input
+    uri = URI(input.split(' ')[1])
+    puts uri
+    Net::HTTP.new(uri.host, uri.port).start do |http|
+      request = Net::HTTP::Get.new uri.request_uri
+      res = http.request request
+      puts res.is_a?(Net::HTTPSuccess)
+      session.puts res.body
+     end
+    session.close
+  end
+end
